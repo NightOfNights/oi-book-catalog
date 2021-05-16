5f1b79fef9172bece24bb5150c1383b2e8967249
Alexey Panov <lexade1802@gmail.com>
2021-05-16 16:05:35 +0700

    add loading spinner, add id field to books objects

--- src/pages/mainPage/mainPage.js
+++ src/pages/mainPage/mainPage.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import { useFirebase } from '../../hooks/useFirebase';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import './mainPage.scss';
 
 const MainPage = () => {
   const { firestore } = useFirebase();
 
   const [books, loading, error] = useCollectionData(
-    firestore.collection('books')
+    firestore.collection('books'),
+    { idField: 'id' }
   );
 
-  console.log(...(books || ''));
+  const loadingIcon = (
+    <LoadingOutlined className="main-page__loading-icon" spin />
+  );
+  if (books) {
+    console.log(books);
+  }
 
   return (
     <div>
       Main Page
       <div>{'books'}</div>
+      {loading ? (
+        <Spin indicator={loadingIcon} />
+      ) : error ? (
+        <div>error</div>
+      ) : (
+        <div>asd</div>
+      )}
     </div>
   );
 };
