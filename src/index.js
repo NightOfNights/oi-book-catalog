14edc907beeacce4cdb4abb7d5a80d8c4a83e6c6
Alexey Panov <lexade1802@gmail.com>
2021-05-16 10:35:14 +0700

    add antd

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import { FirebaseProvider } from './providers/firebaseProvider';
+import 'antd/dist/antd.css';
 import './styles/index.scss';
 
 ReactDOM.render(
