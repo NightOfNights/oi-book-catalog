89f1d531b4b43f9f6702392bef2355a9eff427af
Alexey Panov <lexade1802@gmail.com>
2021-05-18 16:17:47 +0700

    update index.js for components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import BookCatalog from './bookCatalog/bookCatalog';
 import BookCard from './bookCard/bookCard';
+import BookModal from './bookModal/bookModal';
+import NumberSlider from './numberSlider/numberSlider';
 
-export { BookCatalog, BookCard };
+export { BookCatalog, BookCard, BookModal, NumberSlider };
